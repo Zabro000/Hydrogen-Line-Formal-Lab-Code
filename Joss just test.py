efd839f476af6de4a686cda31230cdb5a8ab854e
Zabro000 <reinzabrocki@shaw.ca>
2024-07-24 17:21:48 -0600

    I need a calibration file if I want the figure

---
+++ Joss just test.py
@@ -0,0 +1,25 @@
+import virgo
+
+# Set observation parameters
+obs = {
+    'dev_args': '',
+    'rf_gain': 30,
+    'if_gain': 25,
+    'bb_gain': 18,
+    'frequency': 1420e6,
+    'bandwidth': 2.4e6,
+    'channels': 2048,
+    't_sample': 1,
+    'duration': 60,
+    'loc': '',
+    'ra_dec': '',
+    'az_alt': ''
+}
+
+
+
+# Analyze and export data as csv
+virgo.plot(obs_parameters=obs, n=10, m=25, f_rest=1420.4057517667e6,
+           obs_file='observation.dat',
+           spectra_csv='spectrum.csv', power_csv='time_series.csv',
+           dB=True, plot_file='observation2123.png')
