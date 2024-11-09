388753c1418855975ed62ee726f5a2efb236feba
Zabro <reinzabrocki@shaw.ca>
2024-11-09 16:15:22 -0700

    Commit before merging with final version branch

--- Radio Astronomy Script-1.py
+++ Radio Astronomy Script-1.py
@@ -10,7 +10,7 @@ import shutil
 
 # venv\Scripts\Activate.ps1
 
-### Virgo and radio astronomy varibles ###
+# Virgo and radio astronomy varibles
 observation_time_offset = 4
 
 observing_time = 10 # in seconds
